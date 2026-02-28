d140a0d139824f3f695f8e5c51fa9718b7aee82f
Balu <balu@gmail.com>
2026-02-28 16:31:17 +0530

    Remove unused Script import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { ReduxProvider } from "@/redux/Provider";
 import { SITE_CONFIG } from "@/utils/constants";
