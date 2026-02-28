489df4dfd960391b54907a6ec87c23b68bfa7ad4
Balu <balu@gmail.com>
2026-02-28 13:44:07 +0530

    Add admin app

---
+++ admin-capacitor.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@capacitor/cli').CapacitorConfig} */
+module.exports = {
+  appId: 'com.watchdbhd.admin',
+  appName: 'Watchdb Admin',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+    url: 'https://watchdbhd.vercel.app/admin',
+  },
+};
