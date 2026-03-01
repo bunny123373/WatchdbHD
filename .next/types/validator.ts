1ea6a4daa72d58c28c045e8045a7609462c21439
Balu <balu@gmail.com>
2026-03-01 07:34:06 +0530

    Fix ReduxProvider for SSR

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -164,6 +164,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/api/embed/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/embed">> = Specific
+  const handler = {} as typeof import("../../src/app/api/embed/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/api/notifications/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/notifications">> = Specific
