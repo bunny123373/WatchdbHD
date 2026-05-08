cbbce3d1f77888e3325835a64bf8be18dec476ee
steveharrington-glitch <korrabalanna333@gmail.com>
2026-05-08 08:32:42 +0530

    changes

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -191,6 +191,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/api/admin/users/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/admin/users">> = Specific
+  const handler = {} as typeof import("../../src/app/api/admin/users/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/api/admin/verify/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/admin/verify">> = Specific
